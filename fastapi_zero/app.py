0d4c27ed46f12fa47225c984203f65d9a334a304
amanmdest <amanmdest@gmail.com>
2025-05-14 18:31:40 -0300

    Adcionando schema de mensagem

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Bala')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Sub-Mundo!'}
